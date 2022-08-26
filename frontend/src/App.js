80f8fd48a9d39f773b16bfbd7746115872a322e7
Gerard <irjecogerardo@gmail.com>
2022-08-25 21:36:48 -0600

    dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,8 +5,7 @@ import {
   Route,Routes
 } from "react-router-dom";
 import Login from './components/Login/login';
-
-
+import Dashboard from './components/Dashboard/dashboard';
 import AgregarAmigos from './components/AgregarAmigos/AgregarAmigos';
 
 
@@ -17,6 +16,7 @@ function App() {
      <Routes>
       <Route path="/" element={<Login/>} exact/>
       <Route path="/agregar_amigos" element={<AgregarAmigos/>} exact/>
+      <Route path="/dashboard" element={<Dashboard/>} exact/>
     </Routes>
     </BrowserRouter>
     </div>
