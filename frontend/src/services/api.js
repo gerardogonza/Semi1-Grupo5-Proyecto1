4f2b4f3a7ab9847d595e216054c513eb9f0ed968
Gerard <irjecogerardo@gmail.com>
2022-09-14 15:43:11 -0600

    api

---
+++ frontend/src/services/api.js
@@ -0,0 +1,9 @@
+const URL = 'http://127.0.0.1:5000';
+
+export const parimpar='PAROIMPAR';
+export const fibonacci='FIBO';
+export const alrevez='ALREVEZ';
+export const potencia='POTENCIA';
+export const raiz='RAIZ';
+
+
