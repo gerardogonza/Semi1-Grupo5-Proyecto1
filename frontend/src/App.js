17434c572cfbc7e9fc9710d48e27478a5f4dfad8
Adriana <adriana.d@hotmail.com>
2022-09-14 19:38:04 -0600

    se agregó la vista de editar archivos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Dashboard from "./components/Dashboard/dashboard";
 import Login from "./components/Login/login";
 import AgregarAmigos from "./components/AgregarAmigos/AgregarAmigos";
 import VerArchivos from "./components/VerArchivos/VerArchivos";
+import EditarArchivo from "./components/EditarArchivo/EditarArchivo";
 import Barra from "./components/Barra";
 
 function App() {
@@ -21,6 +22,7 @@ function App() {
           <Route path="/dashboard" element={<div><Barra/> <Dashboard /></div> } exact />
           <Route path="/cargar_archivo" element={<div><Barra/> <CargarArchivo /></div> } exact />
           <Route path="/ver_archivo" element={<div><Barra/> <VerArchivos /></div> } exact />
+          <Route path="/editar_archivo" element={<div><Barra/> <EditarArchivo/></div> } exact />
         </Routes>
       </BrowserRouter>
     </div>
